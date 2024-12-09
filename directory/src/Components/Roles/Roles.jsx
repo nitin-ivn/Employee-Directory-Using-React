eaac67f6bcd69da2d62226faac3927288a5823ce
nitin-ivn <ivnsainitin@gmail.com>
2024-12-09 22:17:30 +0530

    role

--- directory/src/Components/Roles/Roles.jsx
+++ directory/src/Components/Roles/Roles.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Roles() {
   return (
-    <div>Roles</div>
+    <div className='home'>
+      <div className='d-flex title justify-content-between'>
+        <div className='d-flex flex-column'>
+          <h5 className='font-header'>Roles</h5>
+          <p className='font-p'>Find all of your company's employee accounts and thier associated roles</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
